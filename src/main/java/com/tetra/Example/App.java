faa9fc93a396bc8569a22fcb449e01e6031820d4
kuppireddigari swetha <kuppireddyswetha@gmail.com>
2019-12-09 07:31:45 -0800

    branch creation

--- src/main/java/com/tetra/Example/App.java
+++ src/main/java/com/tetra/Example/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.print("hoiiii");
-    }
+        System.out.print("hoiiii  ");
+    }   
 }
